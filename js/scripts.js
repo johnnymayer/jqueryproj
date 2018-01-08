8a648dd1c526186b0dbb89dcd4fea09601ecb2c3
Guest User <Guest@Epicodus-7D.local>
2018-01-08 14:55:57 -0800

    Add new functions in jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,51 @@ $(document).ready(function() {
     alert("This is a paragraph.");
   });
 
-  $("img").click(function() {
-    alert("This is an image.");
+  $("body").hover(function() {
+
+    $("body").css("transform", "rotate("+i+"deg)");
   });
 });
+
+/*
+$('img').each(function () {
+  var hovered = false;
+  var loop = window.setInterval(function () {
+    if (hovered) {
+      $("img").css("transform", "rotate("+i+"deg)");
+      i = i + 1;
+    }
+  }, 250);
+
+  $(this).hover(
+    function () {
+      hovered = true;
+    },
+    function () {
+      hovered = false;
+    }
+  );
+});
+
+
+var i = 0;
+function doSomethingRepeatedly(){
+  $("img").css("transform", "rotate("+i+"deg)");
+  i = i + .01;
+ }
+
+ var intervalId;
+
+ $(document).ready(function () {
+
+   $("img").ready(function () {
+     var intervalDelay = 1;
+     // call doSomethingRepeatedly() function repeatedly with 10ms delay between the function calls
+     intervalId = setInterval(doSomethingRepeatedly, intervalDelay);
+   }, function () {
+     // cancel calling doSomethingRepeatedly() function repeatedly
+     clearInterval(intervalId);
+   });
+
+ });
+*/
