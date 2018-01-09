87a85dcca4acab7bed3d941f2a92957a82839cec
Johnny Mayer <johnnymayer@gmail.com>
2018-01-08 19:55:37 -0800

    Add letterOne, letterTwo, sentence3 functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,79 +2,26 @@
 
 var sentence = prompt("Please enter a sentence.");
 
-var sentence2 = sentence
-  .replace(/^(\w)/, function(v) { return v.toUpperCase(); })
-  .replace(/\w(?=[^\w]*$)/, function(v) { return v.toUpperCase();
+var letterOne = sentence.charAt(0).toUpperCase();
+var letterTwo = sentence.charAt(sentence.length-1).toUpperCase();
 
-});
+var sentence3 = letterOne+letterTwo;
 
 /*
 var firstLetter = sentence2().empty(/^(\w)/, function(v) { return v.toUpperCase(); })
 var secondLetter = sentence2().empty(/\w(?=[^\w]*$)/, function(v) { return v.toUpperCase(); })
 var sentence3 = firstLetter + secondLetter;
+*/
 
 $(document).ready(function() {
   $("body").append(sentence3);
 });
-  //Create another function that will reverse the order of these two letters and return the result.
-    //match and replace $1 for $2 and vice versa
-    //can you store matches as variables
 
-
-/*
-$(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("body").hover(function() {
-
-    $("body").css("transform", "rotate("+i+"deg)");
-  });
-});
-
-$('img').each(function () {
-  var hovered = false;
-  var loop = window.setInterval(function () {
-    if (hovered) {
-      $("img").css("transform", "rotate("+i+"deg)");
-      i = i + 1;
-    }
-  }, 250);
-
-  $(this).hover(
-    function () {
-      hovered = true;
-    },
-    function () {
-      hovered = false;
-    }
-  );
-});
-
-
-var i = 0;
-function doSomethingRepeatedly(){
-  $("img").css("transform", "rotate("+i+"deg)");
-  i = i + .01;
- }
-
- var intervalId;
-
- $(document).ready(function () {
-
-   $("img").ready(function () {
-     var intervalDelay = 1;
-     // call doSomethingRepeatedly() function repeatedly with 10ms delay between the function calls
-     intervalId = setInterval(doSomethingRepeatedly, intervalDelay);
-   }, function () {
-     // cancel calling doSomethingRepeatedly() function repeatedly
-     clearInterval(intervalId);
-   });
-
- });
-*/
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
